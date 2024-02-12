2834df27a082130c508ee775a5998a992af96db6
gmateu <gmateu@esliceu.com>
2024-02-12 13:10:43 +0100

    surface rate

--- func_triangular_arg.py
+++ func_triangular_arg.py
@@ -15,8 +15,12 @@ def structure_trading_pairs(pairs,limit=100):
         a_contract = pair_a["id"]
         a_token_0_decimals=pair_a["token0"]["decimals"]
         a_token_1_decimals=pair_a["token1"]["decimals"]
-        a_token_0_price=pair_a["token0Price"]
-        a_token_1_price=pair_a["token1Price"]
+        try:
+            a_token_0_price=pair_a["token0Price"]
+            a_token_1_price=pair_a["token1Price"]
+        except:
+            a_token_0_price=0
+            a_token_1_price=0
 
         #put (A into a box for checking at b
         a_pair_box=[a_base,a_quote]
@@ -31,8 +35,12 @@ def structure_trading_pairs(pairs,limit=100):
             b_contract = pair_b["id"]
             b_token_0_decimals=pair_b["token0"]["decimals"]
             b_token_1_decimals=pair_b["token1"]["decimals"]
-            b_token_0_price=pair_b["token0Price"]
-            b_token_1_price=pair_b["token1Price"]
+            try:
+                b_token_0_price=pair_b["token0Price"]
+                b_token_1_price=pair_b["token1Price"]
+            except:
+                b_token_0_price=0
+                b_token_1_price=0
 
             #get third paid (c)
             if a_pair != b_pair:
@@ -47,8 +55,13 @@ def structure_trading_pairs(pairs,limit=100):
                         c_contract = pair_c["id"]
                         c_token_0_decimals=pair_c["token0"]["decimals"]
                         c_token_1_decimals=pair_c["token1"]["decimals"]
-                        c_token_0_price=pair_c["token0Price"]
-                        c_token_1_price=pair_c["token1Price"]
+                        try:
+                            c_token_0_price=pair_c["token0Price"]
+                            c_token_1_price=pair_c["token1Price"]
+                        except:
+                            c_token_0_price=0
+                            c_token_1_price=0
+
 
                         #count number on (C) items
                         if c_pair != a_pair and c_pair != b_pair:
@@ -110,7 +123,313 @@ def structure_trading_pairs(pairs,limit=100):
 
 
 
+def calc_triangular_arb_surface_rate(t_pair,min_rate):
+    # Set variables
+    min_surface_rate = min_rate
+    surface_dict = {}
+    pool_contract_2 = ""
+    pool_contract_3 = ""
+    pool_direction_trade_1 = ""
+    pool_direction_trade_2 = ""
+    pool_direction_trade_3 = ""
 
+    # Calculate looping through forward and reverse rates
+    direction_list = ["forward", "reverse"]
+    for direction in direction_list:
+
+        # Set pair info
+        a_base = t_pair["aBase"]
+        a_quote = t_pair["aQuote"]
+        b_base = t_pair["bBase"]
+        b_quote = t_pair["bQuote"]
+        c_base = t_pair["cBase"]
+        c_quote = t_pair["cQuote"]
+        print(f"{a_base}")
+
+        # Set price info
+        a_token_0_price = float(t_pair["aToken0Price"])
+        a_token_1_price = float(t_pair["aToken1Price"])
+        b_token_0_price = float(t_pair["bToken0Price"])
+        b_token_1_price = float(t_pair["bToken1Price"])
+        c_token_0_price = float(t_pair["cToken0Price"])
+        c_token_1_price = float(t_pair["cToken1Price"])
+
+        # Set address info
+        a_contract = t_pair["aContract"]
+        b_contract = t_pair["bContract"]
+        c_contract = t_pair["cContract"]
+
+        # Set variables
+        starting_amount = 1
+        acquired_coin_t2 = 0
+        acquired_coin_t3 = 0
+        calculated = 0
+
+        swap_1 = 0
+        swap_2 = 0
+        swap_3 = 0
+        swap_1_rate = 0
+        swap_2_rate = 0
+        swap_3_rate = 0
+
+        # Assume start with aBase if forward
+        if direction == "forward":
+            swap_1 = a_base
+            swap_2 = a_quote
+            swap_1_rate = a_token_1_price
+            pool_direction_trade_1 = "baseToQuote"
+
+        # Assume start with aQuote if forward
+        if direction == "reverse":
+            swap_1 = a_quote
+            swap_2 = a_base
+            swap_1_rate = a_token_0_price
+            pool_direction_trade_1 = "quoteToBase"
+
+        # Place first trade
+        pool_contract_1 = a_contract
+        acquired_coin_t1 = starting_amount * swap_1_rate
+
+        # Forward: check if aQuote (acquired coin) matches bQuote //////////
+        if direction == "forward":
+            if a_quote == b_quote and calculated == 0:
+                swap_2_rate = b_token_0_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "quoteToBase"
+                pool_contract_2 = b_contract
+
+                # Forward: check if bBase (acquired coin) matches cBase
+                if b_base == c_base:
+                    swap_3 = c_base
+                    swap_3_rate = c_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = c_contract
+
+                # Forward: check if bBase (acquired coin) matches cQuote
+                if b_base == c_quote:
+                    swap_3 = c_quote
+                    swap_3_rate = c_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = c_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Forward: check if aQuote (acquired coin) matches bBase //////////
+        if direction == "forward":
+            if a_quote == b_base and calculated == 0:
+                swap_2_rate = b_token_1_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "baseToQuote"
+                pool_contract_2 = b_contract
+
+                # Forward: check if bBase (acquired coin) matches cBase
+                if b_quote == c_base:
+                    swap_3 = c_base
+                    swap_3_rate = c_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = c_contract
+
+                # Forward: check if bBase (acquired coin) matches cQuote
+                if b_quote == c_quote:
+                    swap_3 = c_quote
+                    swap_3_rate = c_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = c_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Forward: check if aQuote (acquired coin) matches cQuote //////////
+        if direction == "forward":
+            if a_quote == c_quote and calculated == 0:
+                swap_2_rate = c_token_0_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "quoteToBase"
+                pool_contract_2 = c_contract
+
+                # Forward: check if cBase (acquired coin) matches bBase
+                if c_base == b_base:
+                    swap_3 = b_base
+                    swap_3_rate = b_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = b_contract
+
+                # Forward: check if cBase (acquired coin) matches bQuote
+                if c_base == b_quote:
+                    swap_3 = b_quote
+                    swap_3_rate = b_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = b_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Forward: check if aQuote (acquired coin) matches cBase //////////
+        if direction == "forward":
+            if a_quote == c_base and calculated == 0:
+                swap_2_rate = c_token_1_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "baseToQuote"
+                pool_contract_2 = c_contract
+
+                # Forward: check if cQuote (acquired coin) matches bBase
+                if c_quote == b_base:
+                    swap_3 = b_base
+                    swap_3_rate = b_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = b_contract
+
+                # Forward: check if cQuote (acquired coin) matches bQuote
+                if c_quote == b_quote:
+                    swap_3 = b_quote
+                    swap_3_rate = b_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = b_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Reverse: check if aBase (acquired coin) matches bBase //////////
+        if direction == "reverse":
+            if a_base == b_base and calculated == 0:
+                swap_2_rate = b_token_1_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "baseToQuote"
+                pool_contract_2 = b_contract
+
+                # Forward: check if bQuote (acquired coin) matches cQuote
+                if b_quote == c_quote:
+                    swap_3 = c_quote
+                    swap_3_rate = c_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = c_contract
+
+                # Forward: check if bQuote (acquired coin) matches cBase
+                if b_quote == c_base:
+                    swap_3 = c_base
+                    swap_3_rate = c_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = c_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Reverse: check if aBase (acquired coin) matches bQuote //////////
+        if direction == "reverse":
+            if a_base == b_quote and calculated == 0:
+                swap_2_rate = b_token_0_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "quoteToBase"
+                pool_contract_2 = b_contract
+
+                # Forward: check if bBase (acquired coin) matches cQuote
+                if b_base == c_quote:
+                    swap_3 = c_quote
+                    swap_3_rate = c_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = c_contract
+
+                # Forward: check if bBase (acquired coin) matches cBase
+                if b_base == c_base:
+                    swap_3 = c_base
+                    swap_3_rate = c_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = c_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Reverse: check if aBase (acquired coin) matches cBase //////////
+        if direction == "reverse":
+            if a_base == c_base and calculated == 0:
+                swap_2_rate = c_token_1_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "baseToQuote"
+                pool_contract_2 = c_contract
+
+                # Forward: check if cQuote (acquired coin) matches bQuote
+                if c_quote == b_quote:
+                    swap_3 = b_quote
+                    swap_3_rate = b_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = b_contract
+
+                # Forward: check if cQuote (acquired coin) matches bBase
+                if c_quote == b_base:
+                    swap_3 = b_base
+                    swap_3_rate = b_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = b_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Reverse: check if aBase (acquired coin) matches cQuote //////////
+        if direction == "reverse":
+            if a_base == c_quote and calculated == 0:
+                swap_2_rate = c_token_0_price
+                acquired_coin_t2 = acquired_coin_t1 * swap_2_rate
+                pool_direction_trade_2 = "quoteToBase"
+                pool_contract_2 = c_contract
+
+                # Forward: check if cBase (acquired coin) matches bQuote
+                if c_base == b_quote:
+                    swap_3 = b_quote
+                    swap_3_rate = b_token_0_price
+                    pool_direction_trade_3 = "quoteToBase"
+                    pool_contract_3 = b_contract
+
+                # Forward: check if cBase (acquired coin) matches bBase
+                if c_base == b_base:
+                    swap_3 = b_base
+                    swap_3_rate = b_token_1_price
+                    pool_direction_trade_3 = "baseToQuote"
+                    pool_contract_3 = b_contract
+
+                acquired_coin_t3 = acquired_coin_t2 * swap_3_rate
+                calculated = 1
+
+        # Calculate arbitrage results
+        profit_loss = acquired_coin_t3 - starting_amount
+        profit_loss_perc = (profit_loss / starting_amount) * 100 if profit_loss != 0 else 0
+
+        # Format Description
+        trade_description_1 = f"Start with {swap_1} of {starting_amount}. Swap at {swap_1_rate} for {swap_2} acquiring {acquired_coin_t1}."
+        trade_description_2 = f"Swap {acquired_coin_t1} of {swap_2} at {swap_2_rate} for {swap_3} acquiring {acquired_coin_t2}."
+        trade_description_3 = f"Swap {acquired_coin_t2} of {swap_3} at {swap_3_rate} for {swap_1} acquiring {acquired_coin_t3}."
+
+        # Filter for significant opportunity size
+        if profit_loss_perc >= min_surface_rate:
+
+            # Construct Output
+            surface_dict = {
+                "swap1": swap_1,
+                "swap2": swap_2,
+                "swap3": swap_3,
+                "poolContract1": pool_contract_1,
+                "poolContract2": pool_contract_2,
+                "poolContract3": pool_contract_3,
+                "poolDirectionTrade1": pool_direction_trade_1,
+                "poolDirectionTrade2": pool_direction_trade_2,
+                "poolDirectionTrade3": pool_direction_trade_3,
+                "startingAmount": starting_amount,
+                "acquiredCoinT1": acquired_coin_t1,
+                "acquiredCoinT2": acquired_coin_t2,
+                "acquiredCoinT3": acquired_coin_t3,
+                "swap1Rate": swap_1_rate,
+                "swap2Rate": swap_2_rate,
+                "swap3Rate": swap_3_rate,
+                "profitLoss": profit_loss,
+                "profitLossPerc": profit_loss_perc,
+                "direction": direction,
+                "tradeDesc1": trade_description_1,
+                "tradeDesc2": trade_description_2,
+                "tradeDesc3": trade_description_3
+            }
+            return surface_dict
+
+    return surface_dict
 
 
 
--- graphql.py
+++ graphql.py
@@ -1,7 +1,46 @@
 #https://graph.pulsechain.com/subgraphs/name/pulsechain/blocks/graphql?query=%0A++++%23%0A++++%23+Welcome+to+The+GraphiQL%0A++++%23%0A++++%23+GraphiQL+is+an+in-browser+tool+for+writing%2C+validating%2C+and%0A++++%23+testing+GraphQL+queries.%0A++++%23%0A++++%23+Type+queries+into+this+side+of+the+screen%2C+and+you+will+see+intelligent%0A++++%23+typeaheads+aware+of+the+current+GraphQL+type+schema+and+live+syntax+and%0A++++%23+validation+errors+highlighted+within+the+text.%0A++++%23%0A++++%23+GraphQL+queries+typically+start+with+a+%22%7B%22+character.+Lines+that+start%0A++++%23+with+a+%23+are+ignored.%0A++++%23%0A++++%23+An+example+GraphQL+query+might+look+like%3A%0A++++%23%0A++++%23+++++%7B%0A++++%23+++++++field%28arg%3A+%22value%22%29+%7B%0A++++%23+++++++++subField%0A++++%23+++++++%7D%0A++++%23+++++%7D%0A++++%23%0A++++%23+Keyboard+shortcuts%3A%0A++++%23%0A++++%23++Prettify+Query%3A++Shift-Ctrl-P+%28or+press+the+prettify+button+above%29%0A++++%23%0A++++%23+++++Merge+Query%3A++Shift-Ctrl-M+%28or+press+the+merge+button+above%29%0A++++%23%0A++++%23+++++++Run+Query%3A++Ctrl-Enter+%28or+press+the+play+button+above%29%0A++++%23%0A++++%23+++Auto+Complete%3A++Ctrl-Space+%28or+just+start+typing%29%0A++++%23%0A++
 import requests
 import json
-from func_triangular_arg import structure_trading_pairs
+from func_triangular_arg import structure_trading_pairs,calc_triangular_arb_surface_rate
+from pulsexread import get_all_pairs
+import pickle
+
+
+def retriev_pulsex_w3():
+    pairs=get_all_pairs()
+    print(f"len paris {len(pairs)}")
+
+def retriev_pulsex_information():
+    query="""
+
+        {
+  pairs (orderBy: reserveUSD orderDirection:desc first:500) {
+    id
+    reserveUSD
+    reserve0
+    reserve1
+    token0 {
+      id
+      name      
+      decimals
+      symbol
+    }
+    token0Price
+    token1 {
+      id
+      name     
+      decimals
+      symbol
+    }    
+    token1Price
+  }
+}
+    """
+    url="https://graph.pulsechain.com/subgraphs/name/pulsechain/pairs"
+    url="https://graph.pulsechain.com/subgraphs/name/pulsechain/pulsex"
+    req = requests.post(url,json={'query':query})
+    json_dict =json.loads(req.text)
+    return json_dict
 
 
 def retrieve_uniswap_information():
@@ -25,7 +64,53 @@ def retrieve_uniswap_information():
     json_dict =json.loads(req.text)
     return json_dict
 
-if __name__ == "__main__":
-    pairs=retrieve_uniswap_information()
-    structured_pairs=structure_trading_pairs(pairs["data"]["pools"],limit=200)
+def conect_to_uniswap(load_from_blockchain=True):
+    structured_pairs=[]
+    file_name="structured_pairs_uniswap.pkl"
+    if load_from_blockchain:
+        pairs=retrieve_uniswap_information()
+        the_pairs=pairs["data"]["pools"]
+        the_limit=len(the_pairs)
+        structured_pairs=structure_trading_pairs(the_pairs,limit=the_limit)
+        with open(f"pkl/{file_name}.pkl", "wb") as f:
+            pickle.dump(structured_pairs, f)
+    else:
+        structured_pairs = pickle.load( open(f"pkl/{file_name}.pkl", "rb" ))
+
     print(len(structured_pairs))
+
+    for t_pair in structured_pairs:
+        calc_triangular_arb_surface_rate(t_pair)
+
+def conect_to_pulsex(load_from_blockchain=True):
+    structured_pairs=[]
+    if load_from_blockchain:
+        pairs=retriev_pulsex_information()
+        # print(pairs)
+        the_pairs=pairs["data"]["pairs"]
+        print(the_pairs[0:2])
+        the_limit=len(the_pairs)
+        print(f"pairs: {len(the_pairs)}")
+        # print(the_pairs[0])
+        structured_pairs=structure_trading_pairs(the_pairs,limit=the_limit)
+        with open("pkl/structured_pairs_pls.pkl", "wb") as f:
+            pickle.dump(structured_pairs, f)
+    else:
+        #load from pickle
+        structured_pairs = pickle.load( open("pkl/structured_pairs_pls.pkl", "rb" ))
+
+    print(len(structured_pairs))
+    
+    for t_pair in structured_pairs:
+        surf_rate=calc_triangular_arb_surface_rate(t_pair,min_rate=1.5)
+        print(f"{surf_rate} ")
+
+
+    # retriev_pulsex_w3()
+    # print(structured_pairs)
+
+    
+
+if __name__ == "__main__":
+    # conect_to_uniswap(load_from_blockchain=False)
+    conect_to_pulsex(load_from_blockchain=True)
